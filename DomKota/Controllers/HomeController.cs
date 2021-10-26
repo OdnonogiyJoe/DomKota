420beec2cfdf8d8bebd7c60d2686dd729a8fdc3a
Saveliy <kingring2002@mail.ru>
2021-10-26 15:24:33 +1000

    Добавьте файлы проекта.

---
+++ DomKota/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using DomKota.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DomKota.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //Чтоб страницы не болели
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Dishes() 
+        {
+            return View();
+        }
+        public IActionResult Workers()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
