f1158642625bca0f4da0d48c9d76f8c152ff1178
Saveliy <kingring2002@mail.ru>
2021-11-21 22:59:56 +1000

    Начался прилив, пора поднять паруса

--- DomKota/Models/SampleData.cs
+++ DomKota/Models/SampleData.cs
@@ -15,21 +15,51 @@ namespace DomKota
                 context.Menus.AddRange(
                     new Menu
                     {
-                        Name = "iPhone 8",
-                        Description = "Apple",
-                        Price = 600
+                        Name = "Чикибамбони с яйцом",
+                        Description = "Хорошо поджаренный стейк с яйцом, картошкой и рисом, под фирменным соусом",
+                        Price = 10
                     },
                     new Menu
                     {
-                        Name = "Samsung Galaxy Edge",
-                        Description = "Samsung",
-                        Price = 550
+                        Name = "Чирэветки в кляре под сырным одеялом",
+                        Description = "Пойманные в недрах Тихого океана креветки, придавшиеся кляру, с рисом под сырным одеялом",
+                        Price = 20
                     },
                     new Menu
                     {
-                        Name = "Pixel 3",
-                        Description = "Google",
-                        Price = 500
+                        Name = "Итальянцы под пастой",
+                        Description = "Взрощенная на бескрайних полях пшеница, измельченная в муку и объединившаяся с водой, обратилась в спагетти",
+                        Price = 15
+                    },
+                    new Menu
+                    {
+                        Name = "Властелин колец",
+                        Description = "Приятное дополнение к осежающему дюшесу",
+                        Price = 10
+                    },
+                    new Menu
+                    {
+                        Name = "Шоколадное затмение",
+                        Description = "Затмит печали, горести, уныния",
+                        Price = 10
+                    },
+                    new Menu
+                    {
+                        Name = "Царь горы",
+                        Description = "winner winner chicken dinner",
+                        Price = 10
+                    },
+                    new Menu
+                    {
+                        Name = "Дюшес 0,5л",
+                        Description = "Бодрящий напиток",
+                        Price = 3
+                    },
+                    new Menu
+                    {
+                        Name = "Дюшес 1л",
+                        Description = "Бодрящий напиток",
+                        Price = 5
                     }
                 ); ;
                 context.SaveChanges();
